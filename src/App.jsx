3ddbbc906926dfe323c204d0b0cc0d3f5628bdef
spacecentre <akkilalagar05@gmail.com>
2025-06-27 05:46:59 +0000

    feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { ScrollTrigger } from "gsap/all";
+import { NavBar } from "./components";
+import { HeroSection } from "./sections";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const App = () => {
   return (
     <div>
-      {" "}
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <NavBar />
+      <HeroSection />
+      <div className="h-[300vh] border border-green-500">
+        Extra Space for Scrolling
+      </div>
     </div>
   );
 };
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+      <img
+        src="images/navbar/nav-logo.svg"
+        alt="Logo"
+        className="md:w-24 w-20"
+      />
+    </nav>
+  );
+};
+
+export default NavBar;
